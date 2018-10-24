163e5371fd0bdb8bbba32690c0774e1d87c10654
Owen J Sullivan <multiojuice@gmail.com>
2018-10-23 23:11:37 -0700

    Setup one input with the callback routine I think that we should use as for all input here.

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import {Container,} from 'reactstrap';
+
+import InputForm from './InputForm';
 
 class Create extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {};
   }
 
-
+  handleOnChange = (event) => {
+    this.setState({[event.target.id]: event.target.value})
+  }
 
   render() {
-    return <div>Create</div>
+    return (
+      <Container>
+        <InputForm handleOnChange={this.handleOnChange} />
+      </Container>
+    )
   }
 }
 
