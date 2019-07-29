1a1216a067b5d0218b44d812f003c0d2dad352bd
Devin Matte <devinmatte@gmail.com>
2019-07-29 15:05:27 -0400

    Updating nav links for login

--- src/components/Home/DBCard.jsx
+++ src/components/Home/DBCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import {Card, CardBody, CardHeader, CardText, CardTitle} from "reactstrap";
 
 export default ({id, title, description, vmType}) => {
     return (
         <Card>
             <CardHeader>
-                {vmType}
+                <FontAwesomeIcon icon={faDatabase} /> {vmType}
             </CardHeader>
             <CardBody>
                 <Link to={`/db/${id}`}>
