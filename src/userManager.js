8c717a16662f8e45971b2d89a28b98ca20b09f5d
Devin Matte <devinmatte@gmail.com>
2019-10-06 17:00:40 -0400

    Adding env vars and updating scripts

--- src/userManager.js
+++ src/userManager.js
@@ -1,11 +1,11 @@
 import { createUserManager } from 'redux-oidc'
 
 const userManagerConfig = {
-  client_id: 'deadass',
+  client_id: process.env.REACT_APP_SSO_CLIENT_ID,
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
   response_type: 'token id_token',
   scope: 'openid profile',
-  authority: 'https://sso.csh.rit.edu/auth/realms/csh',
+  authority: process.env.REACT_APP_SSO_AUTHORITY,
   silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
   automaticSilentRenew: true,
   filterProtocolClaims: true,
